8d84e758ec5e7b7785485df24a51e37629fda525
bright113gene <bright113gene@gmail.com>
2022-08-03 10:25:57 -0700

    feat: global definition

--- fft.cpp
+++ fft.cpp
@@ -1,1 +1,27 @@
 //! 2022/8/1
+
+#include <bits/stdc++.h>
+
+#define PI (double(6.28318530717958647692528676659005768394)/2)
+#define NN 8192
+#define FOR(i,n0,n) for(int i=n0; i<n; i++)
+
+#define notPow2(x)  ((x)&(x-1))
+
+using namespace std;
+
+double *cos, *sin;
+int *bra;
+
+unsigned int rev_bits(unsigned int idx, unsigned int bitN) {
+  return 0;
+}
+
+bool fft_init(unsigned int N) {
+  return true;
+}
+
+void fft(double *x, double *y, int N) {
+  return;
+}
+
