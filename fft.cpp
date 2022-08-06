d9c0a26bdbd548d0cce309909656fb32068c4b32
bright113gene <bright113gene@gmail.com>
2022-08-06 10:53:53 -0700

    feat: add comments

--- fft.cpp
+++ fft.cpp
@@ -10,9 +10,10 @@
 
 using namespace std;
 
-double *cos, *sin;
-int *bra;
+double *cosv, *sinv;          // accelerate trigonometric computations
+int *bra;                   // accelerate bit-reverse
 
+// Bit-reversal algorithm
 unsigned int rev_bits(unsigned int idx, unsigned int bitN) {
   unsigned int rev = 0;
   FOR(i, 0, bitN) {
@@ -21,6 +22,7 @@ unsigned int rev_bits(unsigned int idx, unsigned int bitN) {
   return rev;
 }
 
+// Allocate dynamic array, init cos/sin/bra array
 bool fft_init(unsigned int N) {
   if (notPow2(N)) return false;
   int m = 0;
@@ -32,7 +34,7 @@ bool fft_init(unsigned int N) {
 
   FOR(i, 0, N) {
     double v = 2*PI*i/N;
-    cosv[i] = cos(v); sinv[i] = sin(v);
+    cosv[i] = cos(v); sinv[i] = sin(v);   // exp(2*pi*j/N) = W_n`j
   }
   FOR(i, 1, N) bra[i] = rev_bits(i, m-1);
   return true;
@@ -72,10 +74,13 @@ void fft(double *x, double *y, int N) {
   return;
 }
 
+// 4. TEST
+// F_sam = 40,000Hz \____`.  Q = 2*PI*f_sig/F_sam = PI/40
+// f_sig = 500Hz    /    '
+// PointNum = 8192
+
 int main(void) {
   freopen("in.in", "r", stdin);
-  freopen("out.out", "w", stdout);
-  
   double data[8192], zero[8192];
   
   fft_init();
